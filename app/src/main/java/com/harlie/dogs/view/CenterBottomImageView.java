ad93755b135cc6107ce4de852e214b567360e5b4
Lee Hounshell <lee.hounshell@gmail.com>
2020-05-28 16:59:29 -0700

    added CenterBottomImageView

---
+++ app/src/main/java/com/harlie/dogs/view/CenterBottomImageView.java
@@ -0,0 +1,64 @@
+package com.harlie.dogs.view;
+
+import android.content.Context;
+import android.graphics.Matrix;
+import android.util.AttributeSet;
+
+public class CenterBottomImageView extends androidx.appcompat.widget.AppCompatImageView {
+
+    public CenterBottomImageView(Context context) {
+        super(context);
+        setup();
+    }
+
+    public CenterBottomImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        setup();
+    }
+
+    public CenterBottomImageView(Context context, AttributeSet attrs,
+                                 int defStyle) {
+        super(context, attrs, defStyle);
+        setup();
+    }
+
+    private void setup() {
+        setScaleType(ScaleType.MATRIX);
+    }
+
+    @Override
+    protected boolean setFrame(int frameLeft, int frameTop, int frameRight, int frameBottom) {
+        if (getDrawable() == null) {
+            return super.setFrame(frameLeft, frameTop, frameRight, frameBottom);
+        }
+        float frameWidth = frameRight - frameLeft;
+        float frameHeight = frameBottom - frameTop;
+
+        float originalImageWidth = (float)getDrawable().getIntrinsicWidth();
+        float originalImageHeight = (float)getDrawable().getIntrinsicHeight();
+
+        float usedScaleFactor = 1;
+
+        if((frameWidth > originalImageWidth) || (frameHeight > originalImageHeight)) {
+            // If frame is bigger than image
+            // => Crop it, keep aspect ratio and position it at the bottom and center horizontally
+
+            float fitHorizontallyScaleFactor = frameWidth/originalImageWidth;
+            float fitVerticallyScaleFactor = frameHeight/originalImageHeight;
+
+            usedScaleFactor = Math.max(fitHorizontallyScaleFactor, fitVerticallyScaleFactor);
+        }
+
+        float newImageWidth = originalImageWidth * usedScaleFactor;
+        float newImageHeight = originalImageHeight * usedScaleFactor;
+
+        Matrix matrix = getImageMatrix();
+        matrix.setScale(usedScaleFactor, usedScaleFactor, 0, 0); // Replaces the old matrix completly
+
+        //comment matrix.postTranslate if you want crop from TOP
+        matrix.postTranslate((frameWidth - newImageWidth) /2, frameHeight - newImageHeight);
+        setImageMatrix(matrix);
+        return super.setFrame(frameLeft, frameTop, frameRight, frameBottom);
+    }
+
+}
